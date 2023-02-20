a2d65b9b0780bbf91b8cf22480d00c2f8de18304
kumarohan0115 <rohankr350@gmail.com>
2023-02-21 00:30:10 +0530

    Day1

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>About</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
