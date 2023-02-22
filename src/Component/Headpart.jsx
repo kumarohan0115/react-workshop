f08407a423c04c17dfee43b50190e33976d5cc9d
kumarohan0115 <rohankr350@gmail.com>
2023-02-22 13:43:55 +0530

    day 3 second commit

--- src/Component/Headpart.jsx
+++ src/Component/Headpart.jsx
@@ -4,26 +4,31 @@ const Headpart = () => {
 
     const headContainer={
         height:'400px',
-        backgroundColor:'green',
+        backgroundColor:'#9acd32',
         display:'flex',
         flexDirection:'column',
         alignItems:'center',
         color:'white'
     }
     const heading={
+        color:'#adff2f',
         fontSize:'3rem'
     }
 
     const btn={
         width:'fit-content',
-        backgroundColor:'black',
-        color:'white'
+        borderRadius:'5px',
+        backgroundColor:'#337ab7',
+        color:'white',
+        padding:'3px',
+        fontSize:'2rem'
+
     }
 
   return (
     <div style={headContainer}>
         <h1 style={heading}>Dramatically Engage</h1>
-        <h3>Objectively innovate empowered manufactured products whereas parallel platforms.</h3>
+        <h3 style={{color:'#adff2f'}}>Objectively innovate empowered manufactured products whereas parallel platforms.</h3>
         <button style={btn}>Engage Now</button>
     </div>
   )
